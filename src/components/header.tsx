bcf72a2c28cee589fdaa0143cb90cc159976546d
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2022-07-31 03:18:41 -0300

    Adjust the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,11 @@
-import { Button, Group, Header as BaseHeader, Text } from "@mantine/core";
+import {
+  Button,
+  Container,
+  createStyles,
+  Group,
+  Header as BaseHeader,
+  Text,
+} from "@mantine/core";
 import NextLink from "next/link";
 
 import type { User } from "@supabase/supabase-js";
@@ -7,16 +14,29 @@ type Props = {
   user: User;
 };
 
+const useStyles = createStyles({
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "100%",
+  },
+});
+
 export const Header = ({ user }: Props) => {
+  const { classes } = useStyles();
+
   return (
-    <BaseHeader height={60} p="md">
-      <Group position="apart">
+    <BaseHeader height={60}>
+      <Container className={classes.inner}>
         <Text>{`${user.email ?? ""}'s notes`}</Text>
 
-        <Button component={NextLink} href="/api/auth/logout">
-          Sign out
-        </Button>
-      </Group>
+        <Group>
+          <Button component={NextLink} href="/api/auth/logout">
+            Sign out
+          </Button>
+        </Group>
+      </Container>
     </BaseHeader>
   );
 };
